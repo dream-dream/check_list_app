0184bce71a459e4944cf13e276af93361eb7fb53
dream-bule <37263385+dream-blue@users.noreply.github.com>
2019-03-28 00:25:02 +0800

    Update settings.py

--- check_list_app/settings.py
+++ check_list_app/settings.py
@@ -116,7 +116,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'test',
         'USER': 'root',
-        'PASSWORD': '123',
+        'PASSWORD': '********',
         'HOST': 'localhost',
         'PORT': '3306',
     }
