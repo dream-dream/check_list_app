aa6607f021e5beaafa6170e392a9ff0a39db1b61
dream-blue <1051921501@qq.com>
2019-04-14 22:52:02 +0800

    correct start-file

--- manage.py
+++ manage.py
@@ -3,14 +3,15 @@ import os, sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from flask_script import Manager, Server  # use that design your own command through function or class method
 
-from .__init__ import app
-from .views.view import api
+from flask_check_list import app
+from flask_check_list.views.view import api
 
 manager = Manager(app)
 manager.add_command("runserver", Server(
     use_debugger=True,
     use_reloader=True,
-    host='0.0.0.0')
+    host='0.0.0.0',
+    port=5000)
                     )
 
 app.register_blueprint(api, url_prefix='/api/v1')
